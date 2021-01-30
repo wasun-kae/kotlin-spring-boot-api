99bf47d1710a52d9b801f5165856951dbeb998b0
Wasun Kaewpradub <>
2021-01-30 16:35:59 +0700

    Fix check alive request path

--- src/main/kotlin/kae/wasun/app/kotlin/boot/controller/HealthController.kt
+++ src/main/kotlin/kae/wasun/app/kotlin/boot/controller/HealthController.kt
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthController {
 
-    @GetMapping("health")
+    @GetMapping("/health")
     fun checkAlive() = "OK"
 }
