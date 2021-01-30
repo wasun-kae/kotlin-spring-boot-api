cf74ce1ccc46b4b5d2d098947c5617d3b53fbe58
Wasun Kaewpradub <>
2021-01-30 16:28:55 +0700

    Add HealthController

---
+++ src/main/kotlin/kae/wasun/app/kotlin/boot/controller/HealthController.kt
@@ -0,0 +1,11 @@
+package kae.wasun.app.kotlin.boot.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("health")
+    fun checkAlive() = "OK"
+}
